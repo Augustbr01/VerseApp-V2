0f03cb98cf75f7b5b00cbdb39b849be16ae06ee5
Augusto Corrêa <augustogatticorrea777@gmail.com>
2026-02-20 23:51:24 -0400

    feat: adjust font sizes in Search component for consistency

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -41,13 +41,13 @@ export default function Search() {
                         </div>
                     </div>
                     <div className="animate-in slide-in-from-bottom duration-700 delay-1000 ml-10 flex ">
-                        <h1 className="font-display inline font-bold text-5xl md:text-6xl lg:text-6xl ">a </h1>
+                        <h1 className="font-display inline font-bold text-5xl md:text-5xl lg:text-6xl ">a </h1>
                         <h1 className="font-display inline font-bold text-5xl text-[#F4C430] md:text-6xl lg:text-6xl ml-3">verdade</h1>
                         
                     </div>
                     <div className="animate-in slide-in-from-bottom duration-700 delay-2000 ml-10 flex ">
-                        <h1 className="font-display inline font-bold text-5xl md:text-6xl lg:text-6xl">e a  </h1> 
-                        <h1 className="font-display inline font-bold text-5xl md:text-6xl lg:text-6xl text-[#F4C430] ml-3 animate-fade-pulse ">vida </h1>
+                        <h1 className="font-display inline font-bold text-5xl md:text-5xl lg:text-6xl">e a  </h1> 
+                        <h1 className="font-display inline font-bold text-5xl md:text-5xl lg:text-6xl text-[#F4C430] ml-3 animate-fade-pulse ">vida </h1>
                         
                     </div>
                     <BibliaSearch />
